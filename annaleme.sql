bb8023de98a3e14a19dcb37b251ca08e4d095985
anna leme <annaleme.tds1.senai@gmail.com>
2024-11-12 16:36:59 -0300

    feat: tabela reservas

--- annaleme.sql
+++ annaleme.sql
@@ -10,3 +10,16 @@ id_quarto SERIAL PRIMARY KEY,
 numero_quarto DECIMAL (5,2) UNIQUE NOT NULL,
 tipo_quarto VARCHAR (100)
 );
+
+
+CREATE TABLE reservas (
+id_reserva SERIAL PRIMARY KEY,
+data_inicio_reserva DATE NOT NULL,
+horario_reserva TIME NOT NULL,
+data_termino_reserva DATE NOT NULL,
+id_hospede INT NOT NULL,
+id_quarto INT NOT NULL,
+reserva_ativa BOOLEAN NOT NULL,
+CONSTRAINT fk_hospede FOREIGN KEY (id_hospede) REFERENCES hospedes(id_hospede),
+CONSTRAINT fk_quarto FOREIGN KEY (id_quarto) REFERENCES quartos(id_quarto)
+);
